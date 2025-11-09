0594ee8334a035f4c89eb46fc4052ebc45cb0cb9
IDGS-903-22002222 <78643@alumnos.utleon.edu.mx>
2025-11-08 19:09:30 -0600

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <NavLink to="/" className="navbar-brand">
+        <NavLink to="/" className="navbar-brand" onClick={closeNavbar}>
           Congreso TIC's
         </NavLink>
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          onClick={toggleNavbar}
+          aria-expanded={isOpen}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <NavLink to="/" className="nav-link">
+              <NavLink to="/" className="nav-link" onClick={closeNavbar}>
                 Inicio
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/participantes" className="nav-link">
+              <NavLink
+                to="/participantes"
+                className="nav-link"
+                onClick={closeNavbar}
+              >
                 Participantes
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/registro" className="nav-link">
+              <NavLink
+                to="/registro"
+                className="nav-link"
+                onClick={closeNavbar}
+              >
                 Registro
               </NavLink>
             </li>
